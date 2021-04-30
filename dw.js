3921f8d37187387cbb4a986a5e4a5d52e88b0f1d
James <75743528+goodtimes983@users.noreply.github.com>
2021-04-29 23:31:43 -0400

    Update dw.js

--- dw.js
+++ dw.js
@@ -1,7 +1,7 @@
 function welcome(){
 var c=document.getElementById("container");
 this.img=document.createElement("img");
-this.img.src="welcomedaniel.png";
+this.img.src="welcomedaniel3.png";
 c.appendChild(this.img);
 this.img.style.height=screen.height;
 this.img.style.width=screen.width;
@@ -13,9 +13,9 @@ function cookie1(){
 
 var d=new Date();
 var t=d.getTime();
-alert(t);
+//alert(t);
 
-alert(document.cookie);
+//alert(document.cookie);
 
 if(document.cookie==""){
 document.cookie = "username=" +t+"; expires=Thu, 18 Dec 2099 12:00:00 UTC; path=/";
@@ -31,4 +31,40 @@ w1.img="welcomedaniel2.png";
 
 }
 
-//cookie1();
+
+
+
+function policy(){
+var img=document.createElement("img");
+img.src="policy.png";
+var c= document.getElementById("box");
+c.appendChild(img);
+img.width=screen.width*0.35;
+img.height=screen.height*0.25;
+c.style.top=screen.height*0.75+"px";
+var button1=document.createElement("button");
+button1.innerHTML="Opt In";
+button1.style.fontSize="large";
+button1.style.background="green";
+c.appendChild(button1);
+button1.onclick=function(){
+
+cookie1();
+
+img.remove();
+button1.remove();
+button2.remove();
+ }
+var button2=document.createElement("button");
+button2.innerHTML="Opt Out";
+button2.style.fontSize="large";
+button2.style.background="red";
+c.appendChild(button2);
+button2.onclick=function(){
+img.remove();
+button1.remove();
+button2.remove();
+ }
+}
+
+policy();
