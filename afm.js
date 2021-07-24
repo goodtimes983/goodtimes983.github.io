619525a5319aabf4342f090a32ee198304dd4625
James <75743528+goodtimes983@users.noreply.github.com>
2021-07-24 14:42:23 -0400

    Add files via upload

---
+++ afm.js
@@ -0,0 +1,94 @@
+function welcome(){
+var c=document.getElementById("container");
+var img=document.createElement("img");
+c.appendChild(img);
+img.src="welcome.png";
+}
+
+w1=new welcome();
+
+function genre(s,a){
+var box=document.getElementById("box");
+box.style.top=100+"px";
+box.style.left=700+"px";
+this.button=document.createElement("button");
+box.appendChild(this.button);
+this.button.innerHTML=s;
+this.button.style.fontSize="xx-large";
+this.button.style.background="pink";
+this.button.style.fontFamily="impact";
+this.button.onclick=function(){
+
+element=document.getElementById("table");
+while (element.firstChild) {
+  element.removeChild(element.firstChild);
+}
+
+
+
+stringa=a;
+stringb="";
+stringc="";
+for(i=0;i<stringa.length;i++){
+if(stringa.charAt(i)==","){
+this.a1= new artist(stringb);
+
+
+
+stringb="";
+ }
+
+else{
+stringb=stringb+stringa.charAt(i);
+ }
+}
+ }
+}
+
+
+
+
+g1 = new genre("Rock","Jim,JimBerts,"); //  ***   All genre objects must start with the genre, and all artists in the parenthesis should end with a ","   ***
+g2 = new genre("Rap","GarFunk,Turksoncakes,");
+g3 = new genre("Hip Hop","back-ave,99 Celsius,");
+g4 = new genre("Techno", "lit,noliter,");
+
+
+
+function artist(s){
+
+var box=document.getElementById("table");
+box.style.top=100+"px";
+box.style.left=800+"px";
+this.button=document.createElement("button");
+box.appendChild(this.button);
+this.button.innerHTML=s;
+this.button.style.fontSize="xx-large";
+this.button.style.background="LimeGreen";
+this.button.style.fontFamily="impact";
+
+var img=document.createElement("img");
+//img.alt="default.png";/////////////
+img.src=s+".png";
+this.button.appendChild(img);
+img.onerror=function(){img.src="default.png";}
+img.style.height=screen.height*0.2;
+img.style.width=screen.width*0.2;
+
+
+img.onclick=function(){
+window.open("afm2.html");//////////////,"_self"
+var r1=new relay(s);/////////////
+ }
+}
+
+function relay(s){
+this.r=s
+var log=document.getElementById("log");
+var button=document.createElement("button");
+log.appendChild(button);
+button.innerHTML=this.r;
+button.style.display="none";
+}
+
+
---
+++ afm2.js
@@ -0,0 +1,85 @@
+function welcome(){
+var c=document.getElementById("container");
+var img=document.createElement("img");
+c.appendChild(img);
+img.src="welcome.png";
+}
+
+w1=new welcome();
+
+function artists(s,a){
+
+s=window.opener.document.getElementById("log").firstChild.innerHTML;
+window.opener.close();
+alert(s);//maybe instead of (s)...(a)?
+
+var box=document.getElementById("box");
+box.style.top=100+"px";
+box.style.left=700+"px";
+this.button=document.createElement("button");
+box.appendChild(this.button);
+this.button.innerHTML=s;
+this.button.style.fontSize="xx-large";
+this.button.style.background="pink";
+this.button.style.fontFamily="impact";
+this.button.onclick=function(){
+
+element=document.getElementById("table");
+while (element.firstChild) {
+  element.removeChild(element.firstChild);
+}
+
+
+
+stringa=a;
+stringb="";
+stringc="";
+for(i=0;i<stringa.length;i++){
+if(stringa.charAt(i)==","){
+this.a1= new album(stringb);
+stringb="";
+ }
+
+else{
+stringb=stringb+stringa.charAt(i);
+ }
+}
+ }
+}
+
+
+
+
+a1 = new artists("Jim","dung,chi,"); //  ***   All artist objects must start with the artist, and all albums in the parenthesis should end with a ","   ***
+a2 = new artists("GarFunk","poop,plow,");
+a3 = new artists("back-ave","fog,oops,");
+a4 = new artists("lit","pops,ok,");
+
+
+
+function album(s){
+var box=document.getElementById("table");
+box.style.top=100+"px";
+box.style.left=800+"px";
+this.button=document.createElement("button");
+box.appendChild(this.button);
+this.button.innerHTML=s;
+this.button.style.fontSize="xx-large";
+this.button.style.background="LimeGreen";
+this.button.style.fontFamily="impact";
+
+var img=document.createElement("img");
+img.src=s+".png";
+this.button.appendChild(img);
+img.onerror=function(){img.src="default.png";}
+img.style.height=screen.height*0.2;
+img.style.width=screen.width*0.2;
+
+
+img.onclick=function(){
+window.open("afm2.html","_self");//////////////
+
+ }
+}
+
+
