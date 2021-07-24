ba5024bb545ac2cf41d71bebe438818e4d15ef74
James <75743528+goodtimes983@users.noreply.github.com>
2021-07-24 14:48:49 -0400

    Update afm2.js

--- afm2.js
+++ afm2.js
@@ -9,9 +9,9 @@ w1=new welcome();
 
 function artists(s,a){
 
-s=window.opener.document.getElementById("log").firstChild.innerHTML;
+a=window.opener.document.getElementById("log").firstChild.innerHTML;
 window.opener.close();
-alert(s);//maybe instead of (s)...(a)?
+alert(a);//maybe instead of (s)...(a)?
 
 var box=document.getElementById("box");
 box.style.top=100+"px";
