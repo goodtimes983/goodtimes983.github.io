48b4ce0f5ee67003653143d453b8fa0b712ce4de
James <75743528+goodtimes983@users.noreply.github.com>
2021-10-29 06:05:03 -0400

    Add files via upload

---
+++ nrc.js
@@ -0,0 +1,16 @@
+function welcome(){
+var c=document.getElementById("container");
+var img=document.createElement("img");
+img.src="NRC_welcome.png"
+c.appendChild(img);
+}
+
+
+function mobile(){
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ alert("This site is not yet available on a mobile device.");
+ }
+}
+
+welcome();
+mobile();
